3a5a95b61edb25416bb92352f8ab34632c494404
Hemanth-Muppa <muppahemanth@gmail.com>
2025-08-01 14:16:49 +0530

    minor tweakings for vercel

--- server/server.js
+++ server/server.js
@@ -52,7 +52,12 @@ app.use("/api/messages", messageRouter);
 
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, ()=> {
-    console.log("server is running on PORT: " + PORT);
-})
+if(process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, ()=> {
+      console.log("server is running on PORT: " + PORT);
+    })
+}
+
+// Export server for Vercel
+export default server;
